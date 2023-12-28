755e78fbacf6305a346fcb69d15ee86180bfa648
aadehamid <aadehamid@gmail.com>
2023-12-28 17:35:45 +0000

    Updated utils.py

--- utils.py
+++ utils.py
@@ -434,3 +434,103 @@ def keyword_search(query,
 
     result = response['data']['Get']['Articles']
     return result
+
+
+
+def dense_retrieval(query,
+                    client,
+                    results_lang='en',
+                    properties = ["text", "title", "url", "views", "lang", "_additional {distance}"],
+                    num_results=5):
+
+    nearText = {"concepts": [query]}
+
+    # To filter by language
+    where_filter = {
+    "path": ["lang"],
+    "operator": "Equal",
+    "valueString": results_lang
+    }
+    response = (
+        client.query
+        .get("Articles", properties)
+        .with_near_text(nearText)
+        .with_where(where_filter)
+        .with_limit(num_results)
+        .do()
+    )
+
+    result = response['data']['Get']['Articles']
+
+    return result
+
+def search_wikipedia_subset(client, query, num_results = 3, results_lang='en',
+                   properties = ["text", "title", "url", "views", "lang", "_additional {distance}"]):
+
+    nearText = {"concepts": [query]}
+
+    # To filter by language
+    if results_lang:
+        where_filter = {
+        "path": ["lang"],
+        "operator": "Equal",
+        "valueString": results_lang
+        }
+        response = (
+            client.query
+            .get("Articles", properties)
+            .with_where(where_filter)
+            .with_near_text(nearText)
+            .with_limit(5)
+            .do()
+        )
+
+    # Search all languages
+    else:
+        response = (
+            client.query
+            .get("Articles", properties)
+            .with_near_text(nearText)
+            .with_limit(5)
+            .do()
+        )
+
+
+    result = response['data']['Get']['Articles']
+
+    return result
+
+
+
+def generate_given_context(query, weav_client, co_client ):
+
+    results = search(client, query, results_lang='en' )
+
+    title = results[0]['title']
+    context = results[0]['text']
+
+
+
+    prompt = f"""
+    You are a useful AI trained to answer questions based on the context your are provided.
+    Use the Context Information provided below to answer the questions "{query}". If the answer to
+    the question is in the context, extract it and print it. If it's not contained in the provided
+    information, say "I do not know".
+    ---
+    Context information about {title}:
+    Context: {context}
+    End of Context Information
+    ---
+    Question: {query}
+    """
+
+    # to answer from the Context Information
+    prediction = co_client.generate(
+        prompt=prompt,
+        max_tokens=50,
+        # model='command-light',
+        # temperature=0.3,
+        num_generations=5)
+
+
+    return prediction, context_title, context_text
