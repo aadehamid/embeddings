57f80fb58abeaeaf69dea082bb2b91456c2a68cd
Hamid Adesokan <aadehamid@gmail.com>
2024-02-24 14:34:38 -0600

    Updated the main file

--- RAG/agent/src/main.py
+++ RAG/agent/src/main.py
@@ -10,35 +10,27 @@ openai.api_key = os.getenv("OPENA_AI_KEY")
 from pathlib import Path
 import pandas as pd
 
-from llama_index.core.query_engine import PandasQueryEngine
+# from llama_index.core.query_engine import PandasQueryEngine
 # from prompt import new_prompt, instruction_str, context
+from prompt import wiki_qa_prompt, context
 
 from prompt import (
     context, instruction_str,
     new_prompt)
 from note_engine import note_engine
-from load_index import canada_engine, text_to_query_engine
+from load_index import (get_sentence_window_query_engine,
+                        get_index,
+                        text_to_query_engine)
+
+from prompt import wiki_qa_prompt, context
+from note_engine import note_engine
+
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import Settings
 from llama_index.core.indices.struct_store import NLSQLTableQueryEngine
-
-
-# %%
-filepath = Path.joinpath(Path.cwd().parent, 'agent_data', 'WorldPopulation2023.csv')
-population = pd.read_csv(filepath)
-dbpath = str(Path(os.path.join(os.path.dirname(os.getcwd()), 'agent_data',
-                'WorldPopulation2023.db')))
-# Connect to sqlite db
-conn = sqlite3.connect(dbpath)
-engine = create_engine("sqlite:///" + dbpath)
-# %%
-# population_query_engine = PandasQueryEngine(
-#     df=population, verbose=True, instruction_str=instruction_str
-# )
-# population_query_engine.update_prompts({"pandas_prompt": new_prompt})
 
 # %%
 model_name = "gpt-3.5-turbo"
@@ -49,12 +41,27 @@ Settings.llm = llm
 Settings.embed_model = embed_model
 table_name = "population"
 
-_, sql_database = text_to_query_engine(model_name, embedding_model_name, table_name, engine)
+# %%
+dbpath = str(Path(os.path.join((os.getcwd()), 'RAG', 'agent',  'agent_data', 'WorldPopulation2023.db')))
+print(dbpath)
+# Connect to sqlite db
+conn = sqlite3.connect(dbpath)
+engine = create_engine("sqlite:///" + dbpath)
 
-# Set up text2SQL prompt
-sql_query_engine = NLSQLTableQueryEngine(
-    sql_database=sql_database,
-    tables=[table_name],)
+sql_query_engine = text_to_query_engine(model_name,
+                                        embedding_model_name,
+                                        all_table_names = [table_name],
+                                        engine = engine)
+
+# %%
+
+chromapath = str(Path(os.path.join((os.getcwd()), 'RAG', 'agent',  'agent_data', 'chroma_db')))
+collection_name = 'canada'
+index = get_index(chromapath, collection_name, nodes = None)
+
+canada_engine = get_sentence_window_query_engine(index)
+canada_engine.update_prompts({"wikipedia_canada": wiki_qa_prompt})
+canada_engine.update_prompts({"response_synthesizer:text_qa_template": wiki_qa_prompt})
 # %%
 tools = [
     note_engine,
@@ -77,6 +84,7 @@ tools = [
 llm = OpenAI(model="gpt-3.5-turbo-0613")
 agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
 
+# %%
 while (prompt := input("Enter a prompt (q to quit): ")) != "q":
     result = agent.query(prompt)
     print(result)
